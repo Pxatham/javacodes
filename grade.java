3ea05729f8ae34f4e75968c796cd9ec4ea4d38f7
Pratham <pxathamm03@gmail.com>
2026-03-23 23:41:12 +0530

    grade calculator

---
+++ grade.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class grade {
+    public static void main(String []args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your marks");
+        int a=sc.nextInt();
+            if(a>=90)
+            {
+                System.out.println("Your grade is A");
+            }
+            else if(a>=80)
+            {
+                System.out.println("Your grade is B");
+            }
+            else if(a>=70)
+            {
+                System.out.println("Your grade is C");
+            }
+            else if(a>=60)
+            {
+                System.out.println("Your grade is D");
+            }
+            else
+            {
+                System.out.println("Your grade is F");
+            }
+        
+    }
+}
