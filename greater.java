308edc3c5e073b521714978078d58731ef6421fe
Pratham <pxathamm03@gmail.com>
2026-03-23 23:30:50 +0530

    first java codes

---
+++ greater.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class greater {
+    public void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(a==b){
+            System.out.print("a is qual to b");
+        }
+        else{
+            if(a>b){
+                System.out.print("a is greater than b");
+            }
+            else{
+                System.out.print("a is smaller than b");
+            }
+        }
+    }
+}
