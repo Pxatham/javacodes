c0b7c16b130c4408c8bb688af7de60ba45a2356b
Pratham <pxathamm03@gmail.com>
2026-03-25 21:39:16 +0530

    pattern print

---
+++ numtriangle.java
@@ -0,0 +1,15 @@
+public class numtriangle {
+    public static void main(String[] args) {
+
+        int n = 4;
+
+        for(int i = 1; i <= n; i++) {   // row
+
+            for(int j = 1; j <= i; j++) { // column
+                System.out.print(i + " "); // print row number
+            }
+
+            System.out.println(); // next line
+        }
+    }
+}
