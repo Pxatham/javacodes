d3a7fae2336e17f062be90389ff6d3196cb21f3d
Pratham <pxathamm03@gmail.com>
2026-04-03 22:58:34 +0530

    sum using loop

---
+++ pract.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class pract {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        int i = 1;
+        int sum = 0;
+
+        // while loop
+        while(i <= n) {
+            sum = sum + i;   // add numbers
+            i++;             // increase counter
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
